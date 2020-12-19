85e60e2159982082ab7d1335b92f228ed4784b90
Denis <dlimarev@wakeapp.ru>
2020-12-19 15:10:37 +0300

    add table

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -3,12 +3,13 @@ USE Lots;
 
 -- TODO create tables
 
-create table PurchaseRequisition
+create table Provider -- Поставщики
 (
+	id bigint unsigned not null primary key,
 	fz varchar(255) not null,
 	published_at datetime not null,
 	nmck varchar(255) not null,
-	provision_amount varchar(255) not null,
+	security_amount varchar(255) not null,
 	winner varchar(255) null,
 	provider_region varchar(255) null,
 	address varchar(255) null,
@@ -25,4 +26,35 @@ create table PurchaseRequisition
 	customer varchar(255) null,
 	win_count_year int default 0 null,
 	bad bool null
+	activity_field  varchar(255) null,
+	phone_number varchar(255) null
+);
+
+create table Purchases -- "База по закупкам"
+(
+	id bigint unsigned not null primary key,
+	fz varchar(255) null,
+	customer varchar(255) null,
+	customer_inn varchar(255) null,
+	customer_region varchar(255) null,
+	bidding_region varchar(255) null,
+	customer_activity_field varchar(255) null,
+	bidding_volume varchar(255) null,
+	bidding_count int null,
+	purchase_target varchar(255) null,
+	registry_bidding_number varchar(255) null,
+	contract_price varchar(255) null,
+	participation_security_amount varchar(255) null,
+	execution_security_amount varchar(255) null,
+	published_at datetime null,
+	requisition_deadline_at datetime null,
+	contract_start_at datetime null,
+	contract_end_at datetime null,
+	playground varchar(255) null
+);
+
+create table PurchaseRequisition -- "Поданные заявки"
+(
+	purchase_id bigint unsigned,
+	provider_id bigint unsigned
 );
